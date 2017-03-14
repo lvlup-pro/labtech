fa5ff666613121ae26730b6101a239397540eafc
Kacper Duras <contact@programmingwizzard.com>
2017-03-14 13:49:58 +0100

    German translation

---
+++ src/locales/de.js
@@ -0,0 +1,24 @@
+window.locale.de = {
+    general: {
+        header: 'Service-Status',
+        refresh: 'Die Liste der Dienste Refreshing nimmt alle <seconds> Sekunden vergeben!',
+    },
+    service: {
+        ok: 'Der Service funktioniert',
+        degraded: "Einige Server funktionieren nicht",
+        offline: "Die Arbeiten an der Integration Services",
+        refreshing: 'Aktualisierung...'
+    },
+    hosts: {
+        host: 'Service',
+        online: 'Dienstleistungen: ',
+        location: "Lage",
+        status: "Status"
+    },
+    host: {
+        online: "Online",
+        degraded: "Geringere Kapazität",
+        offline: "Offline",
+        offlineFrom: "Dienst nicht verfügbar von "
+    }
+};
